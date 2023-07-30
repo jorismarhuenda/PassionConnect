43f4ee77bbddb5e691ac2661dbdaed85324e1f4e
Joris Marhuenda <marhuendajoris@gmail.com>
2023-07-30 12:35:38 +0200

    first commit

---
+++ PassionConnect/ChatDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatDetailView.swift
+//  PassionConnect
+//
+//  Created by marhuenda joris on 30/07/2023.
+//
+
+import SwiftUI
+
+struct ChatDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatDetailView()
+    }
+}
---
+++ PassionConnect/UserInterestsView.swift
@@ -0,0 +1,20 @@
+//
+//  UserInterestsView.swift
+//  PassionConnect
+//
+//  Created by marhuenda joris on 29/07/2023.
+//
+
+import SwiftUI
+
+struct UserInterestsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserInterestsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserInterestsView()
+    }
+}
