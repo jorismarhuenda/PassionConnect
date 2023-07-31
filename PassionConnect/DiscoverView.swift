0b470ae18dba208ce61486e4bf381f02f820b436
Joris Marhuenda <marhuendajoris@gmail.com>
2023-07-31 22:11:55 +0200

    finish discoverView

--- PassionConnect/DiscoverView.swift
+++ PassionConnect/DiscoverView.swift
@@ -6,65 +6,78 @@
 //
 
 import SwiftUI
-import Firebase
-import FirebaseFirestore
-import FirebaseFirestoreSwift
 
 struct DiscoverView: View {
     @ObservedObject var viewModel: FirestoreViewModel
     @State private var currentMatchIndex: Int = 0
     @State private var randomMatch: Match? = nil
+    @State private var isSearching: Bool = false
+    @State private var potentialMatches: [Match] = []
+    @State private var preferredAge: Double = 25
+    @State private var maximumDistance: Double = 50
     
     var body: some View {
+        VStack {
+            if let match = randomMatch {
+                CardView(match: match)
+            } else if potentialMatches.isEmpty {
+                Text("Aucune correspondance potentielle trouvée")
+                    .font(.headline)
+                    .padding()
+            } else {
+                CardView(match: potentialMatches[currentMatchIndex])
+            }
+            
             VStack {
-                if let match = randomMatch {
-                    CardView(match: match)
-                } else if viewModel.potentialMatches.isEmpty {
-                    Text("Aucune correspondance potentielle trouvée")
-                        .font(.headline)
-                        .padding()
-                } else {
-                    CardView(match: viewModel.potentialMatches[currentMatchIndex])
-                }
+                Text("Âge préféré : \(Int(preferredAge))")
+                Slider(value: $preferredAge, in: 18...100, step: 1)
+            }
+            .padding()
+            
+            VStack {
+                Text("Distance maximale (en km) : \(Int(maximumDistance))")
+                Slider(value: $maximumDistance, in: 1...500, step: 1)
+            }
+            .padding()
+            
+            HStack(spacing: 40) {
+                Button(action: dislikeCurrentMatch, label: {
+                    Image(systemName: "x.circle")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                        .foregroundColor(.red)
+                })
+                .disabled(randomMatch != nil || currentMatchIndex >= potentialMatches.count)
                 
-                HStack(spacing: 40) {
-                    Button(action: dislikeCurrentMatch, label: {
-                        Image(systemName: "x.circle")
-                            .resizable()
-                            .frame(width: 44, height: 44)
-                            .foregroundColor(.red)
-                    })
-                    .disabled(randomMatch != nil || currentMatchIndex >= viewModel.potentialMatches.count)
-                    
-                    Button(action: likeCurrentMatch, label: {
-                        Image(systemName: "heart.circle")
-                                                .resizable()
-                                                .frame(width: 44, height: 44)
-                                                .foregroundColor(.green)
-                                        })
-                                        .disabled(randomMatch != nil || currentMatchIndex >= viewModel.potentialMatches.count)
-                                        
-                                        Button(action: findRandomMatch, label: {
-                                            Image(systemName: "shuffle")
-                                                .resizable()
-                                                .frame(width: 44, height: 44)
-                                                .foregroundColor(.blue)
-                                        })
-                                        .disabled(randomMatch != nil || currentMatchIndex >= viewModel.potentialMatches.count)
-                                    }
-                                    .padding()
-                                }
-                                .onAppear {
-                                    viewModel.loadPotentialMatches()
-                                }
-                            }
+                Button(action: likeCurrentMatch, label: {
+                    Image(systemName: "heart.circle")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                        .foregroundColor(.green)
+                })
+                .disabled(randomMatch != nil || currentMatchIndex >= potentialMatches.count)
+                
+                Button(action: findRandomMatch, label: {
+                    Image(systemName: "shuffle")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                        .foregroundColor(.blue)
+                })
+                .disabled(randomMatch != nil || currentMatchIndex >= potentialMatches.count)
+            }
+            .padding()
+        }
+        .onAppear {
+            searchMatches()
+        }
+    }
     
     private func likeCurrentMatch() {
-        guard currentMatchIndex < viewModel.potentialMatches.count else {
+        guard currentMatchIndex < potentialMatches.count else {
             return
         }
         
-        let currentMatch = viewModel.potentialMatches[currentMatchIndex]
+        let currentMatch = potentialMatches[currentMatchIndex]
         viewModel.likeMatch(currentMatch) { error in
             if let error = error {
                 print("Erreur lors du like du match : \(error.localizedDescription)")
@@ -75,12 +88,20 @@ struct DiscoverView: View {
     }
     
     private func findRandomMatch() {
-            viewModel.findRandomMatch { match in
-                self.randomMatch = match
-            }
+        viewModel.findRandomMatch { match in
+            self.randomMatch = match
         }
+    }
     
     private func dislikeCurrentMatch() {
         currentMatchIndex += 1
+    }
+    
+    private func searchMatches() {
+        isSearching = true
+        viewModel.searchMatches(preferredAge: Int(preferredAge), maximumDistance: Int(maximumDistance), interests: []) { matches in
+            potentialMatches = matches
+            isSearching = false
+        }
     }
 }
---
+++ Podfile
@@ -0,0 +1,32 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PassionConnect' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'Google-Mobile-Ads-SDK'
+pod 'FirebaseAnalytics'
+pod 'FirebaseAppDistribution'
+pod 'FirebaseAuth'
+pod 'FirebaseFirestore'
+pod 'FirebaseMessaging'
+pod 'FirebaseStorage'
+pod 'FirebaseInAppMessaging'
+pod 'FirebaseDatabase'
+pod 'FBSDKCoreKit'
+pod 'FBSDKLoginKit'
+pod 'FBSDKShareKit'
+
+  # Pods for PassionConnect
+
+  target 'PassionConnectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PassionConnectUITests' do
+    # Pods for testing
+  end
+
+end
