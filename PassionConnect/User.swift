f082d744b0f2c372fef1cbbe669dc70bed233c84
Joris Marhuenda <marhuendajoris@gmail.com>
2023-08-10 22:36:34 +0200

    Modification du code et résolution de problème

---
+++ PassionConnect/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  PassionConnect
+//
+//  Created by marhuenda joris on 09/08/2023.
+//
+
+import SwiftUI
+
+struct User: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var bio: String
+    var email: String
+    var profileImageName: String
+    var fcmToken: String? // Stocke le token FCM de l'utilisateur pour les notifications push
+    var age: Int
+    var interests: [String]
+    var description: String
+    
+}
+
---
+++ PassionConnect/UserInterests.swift
@@ -0,0 +1,13 @@
+//
+//  UserInterests.swift
+//  PassionConnect
+//
+//  Created by marhuenda joris on 10/08/2023.
+//
+
+import SwiftUI
+
+struct UserInterests: Identifiable {
+    let id = UUID()
+    var interests: [String]
+}
